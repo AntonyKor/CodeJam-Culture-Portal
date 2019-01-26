4c83c4ea033f5996af4c84c81bc88897da97d5c9
AntonyKor <antonykor@gmail.com>
2019-01-26 21:13:49 +0300

    feat: add debounce input

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
+import { DebounceInput } from 'react-debounce-input';
 import PropTypes from 'prop-types';
 
 const SearchInput = ({ searchHeandler, t }) => (
-  <input type="text" onChange={e => searchHeandler(e.target.value)} placeholder={t('search-placeholder')} />
+  <DebounceInput
+    type="text"
+    onChange={e => searchHeandler(e.target.value)}
+    placeholder={t('search-placeholder')}
+    minLength={3}
+    debounceTimeout={300}
+  />
 );
 
 SearchInput.propTypes = {
