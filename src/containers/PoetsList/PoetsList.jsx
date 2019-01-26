2913b364eee1a33e0e20e18c48d2aad45df6202a
AntonyKor <antonykor@gmail.com>
2019-01-26 17:48:24 +0300

    feat: add list of poets

--- src/containers/PoetsList/PoetsList.jsx
+++ src/containers/PoetsList/PoetsList.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import styles from './PoetsList.scss';
 import authors from 'src/data';
+import PoetsListItem from 'src/components/PoetsListItem';
+import styles from './PoetsList.scss';
 
-class PoetsList extends React.Component {
-  render() {
-    const { t } = this.props;
-    return (
-      <div className={styles.wrapper}>
-        {t('hello')} from PoetsList
-        {authors.map(author => (
-          <Link to={`/Poets/${author.id}`}>Go to Poet component with id {author.id}</Link>
-        ))}
-      </div>
-    );
-  }
-}
+const PoetsList = ({ t }) => {
+  let key = 0;
+  return (
+    <div className={styles.wrapper}>
+      <table>
+        <tbody>
+          <tr>
+            <th />
+            <th>{t('name-of-poet')}</th>
+            <th>{t('place-of-birth')}</th>
+          </tr>
+          {authors.map((author) => {
+            key += 1;
+            return <PoetsListItem key={key} id={author.id} name={t(`name${author.id}`)} placeOfBirth={t(`placeOfBirth${author.id}`)} />;
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default withNamespaces()(PoetsList);
