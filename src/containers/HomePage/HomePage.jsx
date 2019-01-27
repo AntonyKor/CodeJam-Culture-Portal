99e99afbffa5461fb0409982078acb1df0f2fc54
vvikota <rymsha.viktor@gmail.com>
2019-01-27 23:51:44 +0300

    fix: delete extra lines

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
-import { Link } from 'react-router-dom';
 import styles from './HomePage.scss';
 import AboutTeam from '../../components/AboutTeam';
 import AuthorDay from '../../components/AuthorDay';
@@ -11,8 +10,6 @@ class HomePage extends React.Component {
     const { t } = this.props;
     return (
       <div className={styles.wrapper}>
-        {t('hello')} from HomePage
-        <Link to="/Poets">Go to PoetsList component</Link>
         <h2>{t('portalTitle')}</h2> 
         <p className={styles.portalDescription}>
            {t('portalDescription')} 
