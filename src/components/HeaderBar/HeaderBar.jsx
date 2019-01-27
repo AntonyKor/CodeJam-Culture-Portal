b6928301f93a19625bce0cf526c8822161a1d973
AntonyKor <antonykor@gmail.com>
2019-01-27 16:29:56 +0300

    refactor: transfer changeLanguage to HeaderBar.jsx

--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -5,9 +5,7 @@ import { NavLink } from 'react-router-dom';
 import styles from './HeaderBar.scss';
 
 
-const HeaderBar = ({
-  changeLanguage, t,
-}) => (
+const HeaderBar = ({ t, i18n }) => (
   <div className={styles.wrapper}>
     <h1>{t('Belаrusian poets')}</h1>
     <nav>
@@ -15,7 +13,7 @@ const HeaderBar = ({
       <span>|</span>
       <NavLink exact to="/Poets" className={styles.navLink} activeClassName={styles.active}>{t('Poets list')}</NavLink>
     </nav>
-    <select onChange={changeLanguage}>
+    <select onChange={({ target: { value } }) => i18n.changeLanguage(value)}>
       <option value="en">English</option>
       <option value="ru">Русский</option>
       <option value="by">Беларускі</option>
@@ -24,7 +22,9 @@ const HeaderBar = ({
 );
 
 HeaderBar.propTypes = {
-  changeLanguage: PropTypes.func.isRequired,
+  i18n: PropTypes.shape({
+    changeLanguage: PropTypes.func,
+  }).isRequired,
   t: PropTypes.func.isRequired,
 };
 
