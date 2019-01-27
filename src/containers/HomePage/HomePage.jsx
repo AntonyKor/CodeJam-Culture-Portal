40bcf6915667bef2554f73f414614f243a66d2d7
vvikota <rymsha.viktor@gmail.com>
2019-01-27 11:09:34 +0300

    feat: add about portal and main style

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import { withNamespaces } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import styles from './HomePage.scss';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class HomePage extends React.Component {
   render() {
     const { t } = this.props;
@@ -10,6 +11,10 @@ class HomePage extends React.Component {
       <div className={styles.wrapper}>
         {t('hello')} from HomePage
         <Link to="/Poets">Go to PoetsList component</Link>
+        <h2>{t('portalTitle')}</h2> 
+        <p className={styles.portalDescription}>
+           {t('portalDescription')} 
+        </p>
       </div>
     );
   }
